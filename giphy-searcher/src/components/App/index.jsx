104737f9dd85734d3fc3516064aea523707abff5
Melrose Ceballos <mcm44@hawaii.edu>
2023-04-15 21:49:00 -0700

    Created search function

--- giphy-searcher/src/components/App/index.jsx
+++ giphy-searcher/src/components/App/index.jsx
@@ -1,27 +1,27 @@
 import './styles.css'
 import DetailPage from '../DetailsPage/details'
 import HomePage from '../HomePage/home'
+import Search from '../SearchPage/search';
 import { Routes, Route, Link } from 'react-router-dom';
 
 function App(){
   return(
     <>
     <header>
-      <h1 className="center">React Gif-y Researcher</h1>
+      <Link className="linkMe" to="/">
+        <h1 className="center">React Gif-y Researcher</h1>
+      </Link>
+      <Link to="/search">Search</Link>
     </header>
-    <nav>
-    <Link className="linkMe" to="/">Take Me Home</Link>
-    {/* // I COMMENTED OUT THE DETAIL BECAUSE I DON'T WANT IT TO BE A PART OF THE NAV */}
-    {/* <Link to="/detail">Detail</Link> */}
-    </nav>
     <main>
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/detail/:id" element={<DetailPage />} />
+        <Route path="/search" element={<Search />}/>
       </Routes>
-
     </main>
     </>
   )
 }
+
 export default App;
---
+++ giphy-searcher/src/components/SearchPage/search.jsx
@@ -0,0 +1,54 @@
+import './styles.css'
+import { useState } from 'react'
+import HomePage from '../HomePage/home'
+
+function Search(props)   {
+    const [query, setQuery] = useState('')
+    const [queryResults, setQueryResults] = useState([])
+
+    const handleQuerySubmit = async (event) => {
+        event.preventDefault()
+        const res = await fetch(`https://api.giphy.com/v1/gifs/search?api_key=gFLjl3lP5EhoxffaamxiQnrmpSxoLQ48&q=${query}&limit=25&offset=0&rating=g&lang=en`)
+        const apiResponse = await res.json()
+        const { data } = apiResponse
+        setQueryResults(data)
+    }
+
+    return( 
+        <>
+            <div className="search-page p-10">
+                <form onSubmit={handleQuerySubmit} className="mt-4 text-center">
+                    <label htmlFor="search" className="block font-medium mb-1">
+                        <h1 className="text-3xl font-bold">Search a GIF</h1>
+                    </label>
+                    <br />
+                    <input
+                        className="p-2 w-[60vw] rounded border border-gray-300 focus:outline-none focus:border-gray-500"
+                        name="search"
+                        placeholder="Baby yoda dancing ... "
+                        value={query}
+                        onChange={event => setQuery(event.target.value)}
+                    />
+                    <button
+                        type="submit"
+                        className="mx-1 px-4 py-2 text-gray-300 hover:text-white hover:bg-gray-800 bg-gray-700 rounded transition-all duration-200"
+                    >
+                        Search
+                    </button>
+                </form>
+
+                <div className="gallery mt-8">
+                    {queryResults.length > 0 && queryResults.map((gifs, i) => {
+                        return <HomePage
+                            key={i}
+                            gifs={gifs}
+                            updateHomePage={props.setHomePage}
+                        />
+                    })}
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Search
